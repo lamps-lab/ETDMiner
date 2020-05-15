77c230315baca4f50a4ca520e47a1ab8a607ab47
Muntabir Choudhury <32687449+choudhurym@users.noreply.github.com>
2020-05-15 03:37:23 -0400

    committed

--- src/classifier/text_classification-crf_bio.py
+++ src/classifier/text_classification-crf_bio.py
@@ -16,21 +16,19 @@ import nltk
 from nltk import word_tokenize
 from nltk import pos_tag
 from sklearn.model_selection import train_test_split
-import pycrfsuite
 import os, os.path, sys
 import glob
 from xml.etree import ElementTree as ET
 import numpy as np
 from sklearn.metrics import classification_report
 from nltk.tag import StanfordPOSTagger
-#import nltk.tag.stanford as st
+import stanza
 
-
-csvfile = open('etd_to_bio.csv', 'w')
-csv_writer = csv.writer(csvfile)
+#csvfile = open('etd_to_bio.csv', 'w')
+#csv_writer = csv.writer(csvfile)
 
 def append_ann(files):
-    xml_files = sorted(glob.glob(files+'\*.xml'))
+    xml_files = sorted(glob.glob(files+'/*.xml'))
     #xml_element_tree = None
     new_data = b""
     for xml_file in xml_files:
@@ -52,15 +50,32 @@ def remove_punct(withpunct):
     return(without_punct)
 
 
-#def extract_features(doc):
-#    return [word2features(doc, i) for i in range(len(doc))]
-#
+def extract_features(doc):
+    return [word2features(doc, i) for i in range(len(doc))]
+
 def get_labels(doc):
     return [label for (token, postag, label) in doc]
     
-file_path = r'C:\Users\Muntabir\nltk_data\corpora\cookbook\clean_data\annotated'
+def nlp_stanza_tokenize(document):
+    tokenize_nlp = stanza.Pipeline('en', processors='tokenize')
+    text_document = tokenize_nlp(document)
+    sent_tokens = ""
+    for i, sentence in enumerate(text_document.sentences):
+        sent_tokens = [token.text for token in sentence.tokens]
+    return (sent_tokens)
+
+def nlp_stanza_pos(tokens):
+    pos_nlp = stanza.Pipeline('en', processors='tokenize,mwt,pos', use_gpu=True, pos_batch_size=3000, tokenize_pretokenized=True)
+    pos_document = pos_nlp((" ").join(tokens))
+    sent_pos = [(word.xpos) for sent in pos_document.sentences for word in sent.words]
+    return sent_pos
+
+    
+    
+file_path = '/Users/muntabir/Documents/Project-Metadata-Extraction/annotated'
 allxmlfiles = append_ann(file_path)
 soup = bs(allxmlfiles, "html5lib")
+
 
 #idenmtify the tagged element
 docs = []
@@ -73,15 +88,13 @@ for d in soup.find_all("document"):
         other_tag = 'O'
         if isinstance(word.name, NoneType) == True:
             withoutpunct = remove_punct(word)
-            temp = word_tokenize(withoutpunct)
-            #word_norm = [word.lower() for word in temp]
+            temp = nlp_stanza_tokenize(withoutpunct)
             for token in temp:
                 tags.append((token, other_tag))
         else:
             prev_tag = other_tag
             withoutpunct = remove_punct(word)
-            temp = word_tokenize(withoutpunct)
-            #word_norm = [word.lower() for word in temp]
+            temp = nlp_stanza_tokenize(withoutpunct)
             for token in temp:
                 #beginning of the token
                 if tags != 'O' and prev_tag == 'O' :
@@ -100,78 +113,96 @@ for d in soup.find_all("document"):
                     prev_tag = tag
                          
         sents = sents + tags
-        docs.append(sents)
+        docs.append(sents)  
         
 data = []
-java_path = r"C:\Program Files\Java\jre1.8.0_181\bin\java.exe"
-os.environ['JAVAHOME'] = java_path
+# # # java_path = "/Users/muntabir/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
+# # # os.environ['JAVAHOME'] = java_path
 
 for i, doc in enumerate(docs):
     tokens = [t for t, label in doc]
-    path_to_model = r"C:\Users\Muntabir\stanford-postagger\models\english-bidirectional-distsim.tagger"
-    path_to_jar = r"C:\Users\Muntabir\stanford-postagger\stanford-postagger.jar"
-    pos_tagger = StanfordPOSTagger(model_filename=path_to_model, path_to_jar=path_to_jar)
-    tagged = pos_tagger.tag(tokens)
-    #tagged = pos_tag(tokens)
-    #print(tagged)
-    
-    data.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
-    print(data)
-    
-#    csv_writer.writerow(data)
-#
-#csvfile.close()
+    #print(tokens)
+    tagged = nlp_stanza_pos(tokens)
+    zipped = zip(tokens, tagged)
+    #print(tuple(zipped))
 
-#def word2features(doc, i):
-#    word = doc[i][0]
-#    postag = doc[i][1]
-#    
-#    features = [
-#            'bias',
-#            'word.lower='+word.lower(),
-#            'word.isupper=%s' % word.isupper(),
-#            'word.istitle=%s' % word.istitle(),
-#            'word.isdigit=%s' % word.isdigit(),
-#            'postag=' + postag
-#            ]
-#    #features for words that are not at the begining of the document
-#    if i > 0:
-#        word1 = doc[i-1][0]
-#        postag1 = doc[i-1][1]
-#        features.extend([
-#            '-1:word.lower=' + word1.lower(),
-#            '-1:word.istitle=%s' % word1.istitle(),
-#            '-1:word.isupper=%s' % word1.isupper(),
-#            '-1:word.isdigit=%s' % word1.isdigit(),
-#            '-1:postag=' + postag1
-#            ])
-#    else:
-#        #indicate that it is the begining of a document
-#        features.append('BOS')
-#    
-#    if i < len(doc)-1:
-#        word1 = doc[i+1][0]
-#        postag1 = doc[i+1][1]
-#        features.extend([
-#            '+1:word.lower=' + word1.lower(),
-#            '+1:word.istitle=%s' % word1.istitle(),
-#            '+1:word.isupper=%s' % word1.isupper(),
-#            '+1:word.isdigit=%s' % word1.isdigit(),
-#            '+1:postag=' + postag1
-#        ])
-#    else:
-#        # Indicate that it is the 'end of a document'
-#        features.append('EOS')
-#        
-#    return features
-        
-#check = word2features(doc,i)
-#print(check)
-        
+    data.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, zipped)])
+    #print(data)
+
+
+def word2features(doc, i):
+    word = doc[i][0]
+    postag = doc[i][1]
     
+    features = [
+            'word.lower='+ word.lower(),
+            #'word[-3]=' + word[-3],
+            #'word[-2]=' + word[-2],
+            'word.isupper=%s' % word.isupper(),
+            'word.istitle=%s' % word.istitle(),
+            'word.isdigit=%s' % word.isdigit(),
+            'postag=' + postag
+            ]
+    #features for words that are not at the begining of the document
+    if i > 0:
+        word1 = doc[i-1][0]
+        postag1 = doc[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:word.isdigit=%s' % word1.isdigit(),
+            '-1:postag=' + postag1
+            ])
+    else:
+        #indicate that it is the begining of a document
+        features.append('BOS')
+        
+    #Features that are not at the end of the document
+    if i < len(doc)-1:
+        word1 = doc[i+1][0]
+        postag1 = doc[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:word.isdigit=%s' % word1.isdigit(),
+            '+1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'end of a document'
+        features.append('EOS')
+        
+    return features
+        
+# check = word2features(doc,i)
+# print(check)
+
+#Split to train and test sets
+X = [extract_features(doc) for doc in data]
+#print(X)
+y = [get_labels(doc) for doc in data]        
+#print(y)    
    
-    
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+#train a CRF model
+from sklearn_crfsuite import CRF
+from sklearn_crfsuite import scorers
+from sklearn_crfsuite import metrics
+from collections import Counter
+
+crf = CRF(
+    algorithm='lbfgs',
+    c1=0.1,
+    c2=0.1,
+    max_iterations=100,
+    all_possible_transitions=True
+)
+crf.fit(X_train, y_train)
+y_pred = crf.predict(X_test)
+
+print(metrics.flat_classification_report(y_test, y_pred))
 
             
                 
