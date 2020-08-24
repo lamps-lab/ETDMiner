950a1507c9de3c6a00449f88c9921056eacc3436
Himarsha Jayanetti <53842088+himarshaj@users.noreply.github.com>
2020-08-23 20:18:36 -0400

    updating the code to obtain output in PNG format

--- src/convertion.sh
+++ src/convertion.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# Change into the "tif" directory to find the input files
+# Change into the "pdf" directory to find the input files
 cd pdf || { echo ERROR: Subdirectory tif not found; exit 1; }
 
-# Loop through all files ending in ".tif"
+# Loop through all files ending in ".pdf"
 for f in *.pdf;
 
 do
@@ -12,7 +12,9 @@ do
 out=${f%%pdf}
 out="../tif/${out}tif"
 
-# Show the command we would run
+# Show the command we would run - TIFF
 gs -q -sDEVICE=tiffg4 -r300 -dBATCH -dPDFFitPage -dNOPAUSE -dFirstPage=1 -dLastPage=1 -sOutputFile="$out" "$f"
 
+# Show the command we would run - PNG
+#gs -q -sDEVICE=png16m -r300 -dBATCH -dPDFFitPage -dNOPAUSE -dFirstPage=3 -dLastPage=3 -sOutputFile="$out" "$f"
 done
