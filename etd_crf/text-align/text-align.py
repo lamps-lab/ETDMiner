dc728ce1757bfad8d975e89aa0b3eee72283bda3
choudhurym <muntabirc@gmail.com>
2021-03-10 22:58:28 -0500

    changed text-align.py and added visual_crf

--- etd_crf/text-align/text-align.py
+++ etd_crf/text-align/text-align.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan  7 18:37:26 2021
-
 @author: Muntabir Choudhury
 """
 
@@ -14,7 +13,6 @@ import glob
 import re
 from xml.etree import ElementTree as ET
 from itertools import zip_longest
-import nltk
 from nltk import word_tokenize
 
 
@@ -32,8 +30,10 @@ def numericalSort(value):
 target_files = sorted(glob.glob(clean), key=numericalSort)
 hocr_files = sorted(glob.glob(noisy_hocr), key=numericalSort)
 
-
+## This function will extract all the position information for each token from the hOCR files 
+# and return it as a list 
 def hocr_parser():
+    list_ = []
     for files in hocr_files:
         with open(files, mode='r', encoding='utf-8') as html_input:
             soup = bs4.BeautifulSoup(html_input,'lxml')
@@ -45,90 +45,182 @@ def hocr_parser():
                 #The coordinates of the bounding box
                 x1,y1,x2,y2 = map(int, title[5:title.find(";")].split())
                 tokens_bbox.append({"x1":x1,"y1":y1,"x2":x2,"y2":y2,"text": words})
-                
-            return tokens_bbox
+            
+            list_.append(tokens_bbox)
+                           
+    return list_
 
+
+## This function will read all the annotated files and return it as a list
 def ann():
+    list_ = []
     for files in target_files:
         new_data = b''
         data = ET.parse(files).getroot()
         temp = ET.tostring(data)
-        new_data = new_data+temp
-    return new_data
-
-
-def parser(text):
-    regex = r'(<\S*?>)*(\w+[^<>]*?\w)(<\S*?>)(\s?|<\S*?>?|\,?|\, ?|\n?)(\n?\w.?[^<]*|[>]?\.*[\s?]*\n)'
-    pattern = re.compile(regex)
-    html = text.decode('utf-8')
-    doc = re.findall(pattern, html)
-    return doc
+        new_data = new_data+temp    
+        list_.append(new_data)
     
+    return list_
 
+## This function will parse all the tags from the annotated text files (i.e. xml files) using regular expression
+# and return it as a list
+def parser(text):
+    list_ = []
+    for sent in text:
+        regex = r'(<\S*?>)*(\w+[^<>]*?\w)(<\S*?>)(\s?|<\S*?>?|\,?|\, ?|\n?)(\n?\w.?[^<]*|[>]?\.*[\s?]*\n)'
+        pattern = re.compile(regex)
+        html = sent.decode('utf-8')
+        doc = re.findall(pattern, html)
+        list_.append(doc)
+    
+    return list_
+    
+## This is a preprocessing function which converts list of tuples to a flat list
 def tuplesTolist(tuple_text):
-    list_of_tuples = list(map(list, tuple_text))
-    flat_list = [item for sublist in list_of_tuples for item in sublist]
-    return flat_list
+    list_ = []
+    for elements in tuple_text:
+        #print(elements)
+        list_of_tuples = list(map(list, elements))
+        flat_list = [item for sublist in list_of_tuples for item in sublist]
+        list_.append(flat_list)
 
+    return list_
+
+## This function will convert list to string
 def listToString(text):
         _string = ""
         return _string.join(text)
 
+## This function will align all the noisy tokens and clean text tokens using edlib python wrapper
 def align(text):
-    stopwords = ['<title>', '</title>', '<author>', '</author>', '<university>', '</university>', '<degree>', '</degree>', '<advisor>', '\n',
-             '</advisor>', '<year>', '</year>', '<program>', '</program>']
-    result_list = [word for word in text if word not in stopwords]
-    token_list = []
-    for items in result_list:
-        tokens = word_tokenize(items)
-        token_list.append(tokens)
+    target_list = []
+    for elements in text:
+        stopwords = ['<title>', '</title>', '<author>', '</author>', '<university>', '</university>', '<degree>', '</degree>', '<advisor>', '\n',
+                     '</advisor>', '<year>', '</year>', '<program>', '</program>']
+        result_list = [word for word in elements if word not in stopwords]
+        token_list = []
+        for items in result_list:
+            tokens = word_tokenize(items)
+            token_list.append(tokens)
     
-    flat_list = [item for sublist in token_list for item in sublist]
-    target_ = [words for words in flat_list]
-    target_ = " ".join(target_)
+        flat_list = [item for sublist in token_list for item in sublist]
+    
+        for target_text in flat_list:
+            target_list.append(target_text)      
+    
+    target_ = " ".join(target_list)
         
-    
-    query_text = hocr_parser()
-    query_ = [texts['text'].replace("\n"," ").strip() for texts in query_text]
-    query_ = " ".join(query_)
-
-    
+    list_query = hocr_parser()
+    query_list = []
+    for l in list_query:
+        for texts in l:
+            query_text = texts['text']
+            query_list.append(query_text)
+   
+    query_ = " ".join(query_list)
+  
     result = edlib.align(query_, target_, task="path", additionalEqualities=[])
     alignment = edlib.getNiceAlignment(result, query_, target_)
     aligned = alignment['target_aligned']
     return aligned
 
+## This function save all the aligned tokens into dictionary
+def token_dict(tokens):
+    lines = []
+    for elements in tokenized:
+        lines.append({"aligned-token":elements})
+    return lines
 
+## This function will map all the aligned tokens with the postion information and return the result as a list of dictionaries
 def merge(lines, pos):
     l3 = [{**u, **v} for u, v in zip_longest(lines, pos, fillvalue={})]
     return l3
 
 
-ann_text = ann()
+def normalized(values):
+    norm = [(float(i) - min(values)) / (max(values) - min(values)) for i in values]
+    return norm
 
-target_text = parser(ann_text)
-#print(target_text)
+class Features:
+    
+    def __init__(self, merged_dict):
+        self.merged_dict = merged_dict
+        #self.normalize = normalize
 
-convert_tuples = tuplesTolist(target_text)
+    def x1_feature(self):
+        x1_list = []
+        for values in self.merged_dict:
+            x1_features = values['x1']
+            x1_list.append(x1_features)
+        return x1_list
+    
+    def y1_feature(self):
+        y1_list = []
+        for values in self.merged_dict:
+            y1_features = values['y1']
+            y1_list.append(y1_features)
+        return y1_list
+    
+    def y2_feature(self):
+        y2_list = []
+        for values in self.merged_dict:
+            y2_features = values['y2']
+            y2_list.append(y2_features)
+        return y2_list
+    
 
-text_align = align(convert_tuples)
+if __name__ == "__main__":
+    ann_text = ann()
+    
+    target_text = parser(ann_text)
 
-tokenized = text_align.split()
-#print(tokenized)
+    convert_tuples = tuplesTolist(target_text)
 
-lines = []
-for elements in tokenized:
-    lines.append({"text":elements})
-#print(lines)
+    text_align = align(convert_tuples)
 
-pos_info = hocr_parser()
+    tokenized = text_align.split()
+    
+    tokens = token_dict(tokenized)
+   
+    ## Reading the hOCR parsed files and saving all the keys to a dictionary
+    pos_info = hocr_parser()
+    pos_list = [item for sublist in pos_info for item in sublist]
+    keys = ['x1','y1','x2','y2']
+    pos = [dict((k, d[k]) for k in keys) for d in pos_list]
 
-keys = ['x1','y1','x2','y2']
-pos = [dict((k, d[k]) for k in keys) for d in pos_info]
-##print(pos[0].values())
-#
-merged = merge(lines, pos)
-print(merged)
+    ## mapping all the position information keys and aligned-token into a one single dictionary by calling the merge function 
+    merged = merge(tokens, pos)
+    #print(merged)
+#    bbox_feature = features(merged)
+#    
+#    #Extracting th
+#    left_margin = normalize(bbox_feature)
+#    
+#    print(left_margin)
+    position_feature = []
+    feature = Features(merged)
+    left_margin = feature.x1_feature()
+    normalized_left_margin = normalized(left_margin)
+    yCoordinate1 = feature.y1_feature()
+    normalized_yCoordinate1 = normalized(yCoordinate1)
+    #print(normalized_yCoordinate1)
+    yCoordinate2 = feature.y1_feature()
+    normalized_yCoordinate2 = normalized(yCoordinate2)
+    
+    #position_feature.append
+    #print(position_feature)
+    
+    print(normalized_yCoordinate2)
+    #lines = same_line()
+    #print(lines)
+
+    
+
+    
+
+        
+    
 
 
 
