2f81992b5a0eb9a0db85e4abc3584664972b55b6
Dennis <zdq198654@gmail.com>
2024-01-08 16:15:18 -0500

    change

--- webcrawler/AirForceInstitute/AIT_shifter.py
+++ webcrawler/AirForceInstitute/AIT_shifter.py
@@ -12,6 +12,7 @@ import urllib.request
 import urllib.response
 import urllib.parse
 import re
+import requests
 
 #@Dennis
 # config = {
@@ -22,7 +23,7 @@ import re
 # }
 
 config = {
-    'user': 'root',
+    'user': 'Dennis',
     'password': '1234',
     'host': 'localhost',  # or '127.0.0.1'
     'database': 'testdb' 
@@ -103,11 +104,107 @@ def insertSubjects(soup, etdid):
             mycursor.close()
             db_connection.close()
 
-def insertETDs(soup):
-    """
-        Extract values first
-    """
-    # Title
+# @Dennis create a new funciton to check if the record exists in the database
+def exists_in_etds(soup):
+    data = extract_all_field(soup)
+    title = data.get('title')
+    pri_identifier = data.get('pri_identifier')
+    second_identifier = data.get('second_identifier')
+    # print(f"exists_in_etds: title:{title}, pri_identifier: {pri_identifier} ")
+    
+    db_connection = mysql.connector.connect(**config)
+    mycursor = db_connection.cursor()
+
+    etd_id = None
+
+    # Check if pri_identifier exists
+    query = "SELECT id FROM etds WHERE pri_identifier = %s"
+    params = (pri_identifier,)
+    mycursor.execute(query, params)
+    result = mycursor.fetchone()
+
+    if result:
+        # pri_identifier exists
+        etd_id = result[0]
+        print("pri_identifier exists ")
+
+    # If pri_identifier does not exist, check second_identifier
+    elif not etd_id:
+        query = "SELECT id FROM etds WHERE second_identifier = %s"
+        params = (second_identifier,)
+        mycursor.execute(query, params)
+        result = mycursor.fetchone()
+
+        if result:
+            # second_identifier exists
+            etd_id = result[0]
+            print("second_identifier exists ")
+
+    # If both pri_identifier and second_identifier do not exist, check title
+    elif not etd_id:
+        query = "SELECT id FROM etds WHERE trim(title) = trim(%s)"
+        params = (title,)
+        mycursor.execute(query, params)
+        result = mycursor.fetchone()
+
+        if result:
+            # title exists
+            etd_id = result[0]
+            print("title exists ")
+
+    # No match found for any identifier
+    mycursor.close()
+    db_connection.close()    
+    
+    return etd_id
+    
+
+# @Dennis create a new funciton used to check if this record has any empty field
+def empty_fields(soup,etdid):
+    required_fields = [
+        'title', 'author', 'advisor', 'year', 'university', 'URI', 'department', 'degree', 'discipline', 'language', 'abstract', 'copyright', 'pri_identifier', 'second_identifier', 'haspdf', 'timestamp_metadata','timestamp_pdf'
+    ]
+    
+    empty_fields = []
+
+    db_connection = mysql.connector.connect(**config)
+    mycursor = db_connection.cursor()
+
+    for field in required_fields:
+        query = f"SELECT COUNT(*) FROM etds WHERE id = {etdid} and {field} IS NULL "
+        mycursor.execute(query)
+        result = mycursor.fetchone()
+
+        if result[0] > 0:
+            empty_fields.append(field)
+
+    mycursor.close()
+    db_connection.close()
+
+    return empty_fields
+    
+# @Dennis  create a new function used to update the empty field
+def update_empty_field(soup,empty_list,etdid) :
+    data = extract_all_field(soup)
+    db_connection = mysql.connector.connect(**config)
+    mycursor = db_connection.cursor()
+    print("start update empty feild")
+    for field in empty_list:
+        value = data.get(field)
+        if value is not None:
+            # Update the field in the etds table
+            query = f"UPDATE etds SET {field} = %s WHERE id = {etdid}"
+            params = (value,)
+            mycursor.execute(query, params)
+            db_connection.commit()
+            print(f"updated the field {field}: {value}")
+    mycursor.close()
+    db_connection.close()     
+    
+           
+# @Dennis create a new function extract_all_field, used to extract all field from matadata, and return a JSON.
+def extract_all_field(soup):
+     # Title
     title = soup.find('div',{'id':'title'})
     # print("title: ",title)
     if title is not None:
@@ -145,7 +242,8 @@ def insertETDs(soup):
         url_p = url.find('p')
         url_br = url_p.find('br')
         # print("url_br: ",url_br)
-        url_final = url_br.find_next_sibling(text = True)
+        # url_final = url_br.find_next_sibling(text = True)
+        url_final = url_br.find_next_sibling(string = True)
         # print("url_final: ",url_final)
         url = url_final.strip()
         print("url: ",url)
@@ -165,7 +263,6 @@ def insertETDs(soup):
                 date = date.split(' ')[1]
     except:
         pass
-            
        
   
     # University
@@ -189,27 +286,81 @@ def insertETDs(soup):
         department = department.find('p').get_text()
     
     # Discipline
+    
     discipline = soup.find('div',{'id': 'bp_categories'})
     if discipline is not None:
         try:
             discipline = discipline.find('p').get_text()
         except:
-            discipline = "Not Found"
+            discipline = None
     else:
-        discipline = "Not Found"
+        discipline = None
+        
+        
+    # CopyRight link @Dennis
+    copyright_link = soup.find('a',{'title': 'Copyright Policy'})
+    if copyright_link is not None:
+        try:
+            copyright_url = copyright_link.get('href')
+        except:
+            copyright_url = None
+    else:
+        copyright_link = None
+        
+    # CopyRight text @Dennis
+    def extract_copyright(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.content
+            soup = BeautifulSoup(content, 'html.parser')
+            copyright_pra = soup.find('div',{'class': 'wpb_wrapper'})
+            if copyright_pra is not None:
+                paragraphs = copyright_pra.find_all('p')
+                copyright = '\n'.join([paragraph.get_text() for paragraph in paragraphs])                
+                
+                return copyright
+            else:
+                return None
+    copyright = extract_copyright(copyright_url)
+    
+    # AFIT Designator @Dennis
+    AFIT = soup.find('div',{'id':'afit_designator'})
+    if AFIT is not None:
+        AFIT = AFIT.find('p').get_text()
+        
+    # DTIC  Accession Number @Dennis
+    DTIC = soup.find('div',{'id':'dtic_accession_number'})
+    if DTIC is not None:
+        DTIC = DTIC.find('p').get_text()
+        
+   
+    
+    data = {
+        'title': title,
+        'author': author,
+        'advisor': advisor,
+        'abstract': abstract,
+        'date': date,
+        'university': university,
+        'degree': degree,
+        'url': url,
+        'language': language,
+        'department': department,
+        'discipline': discipline,        
+        'copyright': copyright,
+        'pri_identifier': AFIT,
+        'second_identifier': DTIC,        
+    }
+    
+    # print(data)
+    return data   
 
-    print('Title:', title)
-    print('Author:', author)
-    print('Advisor:', advisor)
-    print('Abstract:', abstract)
-    print('Date:', date)
-    print('University:', university)
-    print('Degree:', degree)
-    print('URL:', url)
-    print('Language:', language)
-    print('Department:', department)
-    print('Discipline:', discipline)
 
+def insertETDs(soup):
+    """
+        Extract values first
+    """
+   
     """
         Database insertion for ETD table
     """
@@ -218,14 +369,33 @@ def insertETDs(soup):
     # Insert values to database
     mycursor = db_connection.cursor()
     oads_flag = 0
-    sql = "INSERT INTO etds (title, author, advisor, year, university, URI, department, degree, discipline, language, abstract, oadsclassifier) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (title, author, advisor, date, university, url, department, degree, discipline, language, abstract, oads_flag)
+    data = extract_all_field(soup)
+    sql = "INSERT INTO etds (title, author, advisor, year, university, URI, department, degree, discipline, language, abstract, oadsclassifier, copyright, pri_identifier, second_identifier) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s)"
+    val = (
+        data.get('title'), 
+        data.get('author'), 
+        data.get('advisor'), 
+        data.get('date'), 
+        data.get('university'), 
+        data.get('url'), 
+        data.get('department'), 
+        data.get('degree'), 
+        data.get('discipline'), 
+        data.get('language'), 
+        data.get('abstract'), 
+        data.get('oads_flag'),
+        data.get('copyright'),
+        data.get('pri_identifier'),
+        data.get('second_identifier'),
+    )
+    
     mycursor.execute(sql, val)
     db_connection.commit()
 
     etdId = mycursor.lastrowid
     mycursor.close()
     db_connection.close()
+    print("etdId: ",etdId)
 
     return etdId
 
@@ -274,6 +444,81 @@ def insertPDFs(soup, etdid, etdPath, etd_number):
 
     mycursor.close()
     db_connection.close()
+    
+# # @Dennis create a final_pdf_dir function, used to return a  final directory
+# def final_pdf_dir(etdid):
+#     prodDir = os.path.join('../', 'etdrepo')
+#     firstLevelDir = firstLevelDirCalculation(etdid)
+#     secondLevelDir = secondLevelDirCalculation(etdid)
+
+#     pdf_FinalProdDir = prodDir+'/'+firstLevelDir+'/'+secondLevelDir    
+#     pdf_path = pdf_FinalProdDir + '/' + str(etdid) + '.pdf'
+#     return pdf_path
+
+# # @Dennis create a final_html_dir(etdid) function, used to return a  final directory
+# def final_html_dir(etdid):
+#     prodDir = os.path.join('../', 'etdrepo')
+#     firstLevelDir = firstLevelDirCalculation(etdid)
+#     secondLevelDir = secondLevelDirCalculation(etdid)
+
+#     html_FinalProdDir = prodDir+'/'+firstLevelDir+'/'+secondLevelDir    
+#     html_path = html_FinalProdDir + '/' + str(etdid) + '.html'
+#     return html_path
+
+
+# # @Dennis create a function: update_all_empty_field(), When I run it, it will  find the directory: etdrepo -> school folder(name is a int), and  iterate the school folder,  update all the empty fields of the table etds (include haspdf, timestamp);
+# from datetime import datetime  
+# def update_all_empty_field():
+#     final_directory = os.path.join("..", "etdrepo", "058")
+#     # print(final_directory)
+    
+#     final_etds = os.listdir(final_directory)
+#     # print(final_etds)
+    
+#     for item in final_etds:
+#         cur_path = os.path.join(final_directory, item)
+#         cur_files = os.listdir(cur_path)
+#         print(cur_files) 
+        
+#         haspdf = None
+#         pdf_timestamp = None
+#         meta_timestamp = None
+        
+#         for file_name in cur_files:
+#             file_path = os.path.join(cur_path,file_name)
+            
+#             if file_name.lower().endswith('.pdf'):
+#                 print(f"Found PDF in {item}: {file_name}")
+#                 haspdf = 1
+#                 pdf_timestamp = datetime.fromtimestamp(os.path.getmtime(file_path))
+#             elif file_name.lower().endswith('.html'):
+#                 print(f"Found HTML in {item}: {file_name}")
+#                 meta_timestamp = datetime.fromtimestamp(os.path.getmtime(file_path))
+        
+   
+
+
+# @Dennis create a insert_haspdf_timestamp function
+from datetime import datetime    
+def insert_haspdf_timestamp(final_dir,etdid):
+    haspdf = 0
+    pdf_path = final_dir + '/' + str(etdid) + '.pdf'
+    meta_path = final_dir + '/' + str(etdid) + '.html'
+    if os.path.exists(pdf_path):
+        haspdf = 1
+    pdf_timestamp = datetime.fromtimestamp(os.path.getmtime(pdf_path))
+    meta_timestamp = datetime.fromtimestamp(os.path.getmtime(meta_path))
+    
+    db_connection = mysql.connector.connect(**config)
+    mycursor = db_connection.cursor()
+
+    sql = "update etds set haspdf = %s, timestamp_pdf = %s timestamp_meatdata = %s where id = %s"
+    val = (haspdf, pdf_timestamp, meta_timestamp, etdid)
+    mycursor.execute(sql, val)
+    db_connection.commit()
+
+    mycursor.close()
+    db_connection.close()
 
 def moveFileToProductionRepo(etdPath, dbETDId):      
     prodDir = os.path.join('../', 'etdrepo')     
@@ -288,6 +533,7 @@ def moveFileToProductionRepo(etdPath, dbETDId):
     
     if etdPath is not None:
         copyfile(etdPath, etdProduction)
+   
        
 def movehtml(htmlpath, dbETDId):
     prodDir = os.path.join('../', 'etdrepo')     
@@ -322,7 +568,8 @@ def main():
     Step 3: Populate 3 tables
     Step 4: Shift files to production repo 
     """
-    # harvestDirectory = 'UNLV_ETDs'
+    
+    #@Dennis harvestDirectory = 'UNLV_ETDs'
     harvestDirectory = 'AIT_ETDs'
     print(harvestDirectory)
     etddirs = os.listdir(harvestDirectory)
@@ -330,14 +577,15 @@ def main():
     #etddirs = handleSuddenStop(etddirs,'metadc278485') #TODO: Change here to handle sudden production stop   #metadc53494
 
     print("#ETDs:", len(etddirs))
-    for i in range(1,5431):
+    #@Dennis for i in range(1,5431):
+    for i in range(1,6298):
         print('')
         print("Current ETD:", i)
         # if etddir == '1911':
         #     continue
-        """
-            Step 1: Read the files [Directory path may vary based on arrangement]
-        """
+        
+        #    Step 1: Read the files [Directory path may vary based on arrangement]
+       
 
         xmlFilePath = os.path.join(harvestDirectory+'/'+str(i),str(i)+'.html')
         print("xmlFilePath: ",xmlFilePath)
@@ -354,37 +602,51 @@ def main():
 
                 xmlfile = open(xmlFilePath, "r")
                 soup = BeautifulSoup(xmlfile, 'lxml')
-                # print("soup:",soup)
-                etdid = insertETDs(soup) # DONE
-                # insertSubjects(soup, etdid) # DONE
-                insertPDFs(soup, etdid, etdPath, str(1001+i-1)) # Done
                 
-
-                """
-                    Step 3: Shift the ETD to the production repo, rename & place file/folders based on ID
-                """
-                print("etdpath",etdPath)
-                moveFileToProductionRepo(etdPath, etdid) #Works
-                #break # Loop breaks after a run for now
-            else:
-                pass
+                # @Dennis                 
+                is_exist = exists_in_etds(soup) #is_exist will be 0 or the etdid
+                print('is_exist: ',is_exist)
+                if is_exist:
+                    etdid = is_exist
+                    # return a list of empty field
+                    empty_fields_list = []
+                    empty_fields_list = empty_fields(soup,etdid)
+                    print('empty_fields: ',empty_fields_list)
+                    # update the empty field
+                    if not empty_fields_list:
+                        update_empty_field(soup,empty_fields_list,etdid)
+                    
+                else:   
+                    etdid = insertETDs(soup) # DONE
+                    # insertSubjects(soup, etdid) # DONE
+                    insertPDFs(soup, etdid, etdPath, str(1001+i-1)) # Done
+                    
+                    #    Step 3: Shift the ETD to the production repo, rename & place file/folders based on ID
+                    
+                    print("etdpath",etdPath)                
+                    moveFileToProductionRepo(etdPath, etdid) #Works
+            
         for item in _etdPath.glob('*.html'): # Return a list
             htmlPath = item
 
         # Extract and insert table
-            if os.path.exists(xmlFilePath):
+            if os.path.exists(xmlFilePath):                
 
                 
-
-                """
-                    Step 3: Shift the ETD to the production repo, rename & place file/folders based on ID
-                """
+                #    Step 3: Shift the ETD to the production repo, rename & place file/folders based on ID
+                
                 print("path: ",htmlPath)
                 movehtml(htmlPath, etdid) #Works
                 #break # Loop breaks after a run for now
             else:
                 pass
+            
+        # @Dennis insert haspdf and timestamp
+                # final_path = final_dir(etdid)
+                # insert_haspdf_timestamp(final_path, etdid)
                 
+
+
 
 if __name__ == '__main__':
     main()
